8b78d1aa23888c7f11640c745e98c3f5073364d9
Kevin <lexdxw@126.com>
2021-11-16 22:51:07 +0800

    Added example for making websocket

--- src/components/ClippedDrawer/ClippedDrawer.js
+++ src/components/ClippedDrawer/ClippedDrawer.js
@@ -126,7 +126,6 @@ export default function ClippedDrawer({setToken}) {
                         <Route path="/userMaintain" component={UserMaintain} />
                         <Route path="*" component={Dashboard} />
                     </Switch>
-
             </Box>
         </Box>
         </BrowserRouter>
