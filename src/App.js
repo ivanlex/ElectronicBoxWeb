ce548126efcb43f5efde38870369d651dde5587e
Kevin <lexdxw@126.com>
2021-11-18 01:18:22 +0800

    Implement WebSocket Server sent event example

--- src/App.js
+++ src/App.js
@@ -11,18 +11,26 @@ function App() {
 
     const { token, setToken } = useToken();
 
-    if(token) {
+    if(!token) {
         return <Login setToken={setToken} />
     }
 
-    var sock = new SockJS('/notifications');
+    let sock = new SockJS('/notifications');
     let stompClient = Stomp.over(sock);
+    let sessionId = "";
+
     sock.onopen = function() {
         console.log('open');
     }
     stompClient.connect({}, function (frame) {
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/heartbeat', function (greeting) {
+        let url = stompClient.ws._transport.url;
+        url = url.replace(
+            "ws://127.0.0.1:8000/notifications/",  "");
+        url = url.replace("/websocket", "");
+        url = url.replace(/^[0-9]+\//, "");
+        console.log("Your current session is: " + url);
+        sessionId = url;
+        stompClient.subscribe('/topic/heartbeat'+'-user'+sessionId, function (greeting) {
             console.log(greeting);
             //you can execute any function here
             stompClient.send("/app/heartbeat", {}, JSON.stringify({'clientId': '007'}));
